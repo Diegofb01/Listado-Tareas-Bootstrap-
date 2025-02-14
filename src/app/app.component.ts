28d05c7a17380e0474c025beff656321a04299bb
dfb <diegofornellb2001@gmail.com>
2025-02-14 08:35:29 +0100

    Primer commit: Añadir proyecto Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { TareasService } from './services/tareas.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ejercicioListaBootstrap';
+export class AppComponent implements OnInit {
+  
+  listaTareas: string[] = [];
+  nuevaTarea: string = '';
+  currentYear: number = new Date().getFullYear();
+  
+  private _tareasService = inject(TareasService);
+  
+  ngOnInit(): void {
+    this.listaTareas = this._tareasService.getTareas();
+  }
+
+  agregarTarea() {
+    if (this.nuevaTarea.trim()) {
+      this._tareasService.agregarTarea(this.nuevaTarea);
+      this.nuevaTarea = '';
+      this.listaTareas = this._tareasService.getTareas();
+    }
+  }
+
+  eliminarTarea(index: number) {
+    this._tareasService.eliminarTarea(index);
+    this.listaTareas = this._tareasService.getTareas();
+  }
 }
